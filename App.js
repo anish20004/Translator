b49636d2138f639ebfa6a7a3fa7222220c0cc79e
anish20004 <anishgholap2004@gmail.com>
2024-08-05 16:58:41 +0530

    commit changes

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 // Middleware to serve static files
 app.use(express.static(__dirname));
 
@@ -10,7 +9,10 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'homepage.html'));
 });
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
